7ba09a9852d235fe16da3b43627915eaa859c786
Leonardo Gonzalez <omikron.dementia@gmail.com>
2022-05-16 20:36:09 -0500

    Added first homework

--- 1intro/2do/inscribed.py
+++ 1intro/2do/inscribed.py
@@ -4,3 +4,19 @@
 #
 # zeeAlso
 # https://keisan.casio.com/exec/system/1223428152
+
+import math
+
+a = input("Lado A = ")
+b = input("Lado B = ")
+c = input("Lado C = ")
+
+a = float(a)
+b = float(b)
+c = float(c)
+
+s = (a + b + c)/2
+st = math.sqrt(s\*(s-a)\*(s-b)\*(s-c))
+r = st/s
+
+print("\nEl radio del círculo inscrito dentro del triángulo (con área de " + str(st) + ") es de " + str(r))
