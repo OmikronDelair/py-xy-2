f39ee1fa29837ddcd86db0be1cfd9b84f24d5e77
Omikron <omikron.dementia@gmail.com>
2022-05-22 21:14:58 -0500

    Added homework 4.1

--- 4roots/2do/2do1.py
+++ 4roots/2do/2do1.py
@@ -2,3 +2,34 @@
 # please refer to PPT file
 # for exercise
 #
+
+from math import e
+import matplotlib.pyplot as plt
+
+print("Resolver usando uno de los métodos a elegir e^(2-x) -7x = 0")
+
+#Respuesta: x=0.586934
+
+# Método elegido: Sustitución sucesiva
+
+def met_ss(x):
+     f = -e**(2-x)/(-7)
+     return f
+#end
+
+# Solución: se encontró la raíz hasta la iteración 25
+
+ult_ite = float(0)
+fx = [0]
+
+for vez in range(1,26):
+    ult_ite = met_ss(ult_ite)
+    fx.append(ult_ite)
+
+# Resultado
+        
+print ("Raíz:", ult_ite)
+
+# Graficación
+
+plt.plot(fx)
